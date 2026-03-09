00825a821d37a4c50a31d4b25f6818a90d725a56
Ayush Bhor <ayushbhor04@gmail.com>
2026-03-09 20:18:31 +0530

    first commit

---
+++ Day_1.py
@@ -0,0 +1,181 @@
+# DAY - 1 
+
+#EASY 
+
+# Q1.Write a program that prints:
+# My name is Ayush
+# I am 24 years old
+# print("My name is Ayush")
+# print("I am 24 years old") 
+
+# Q2.Store the following in variables and print them:
+# Name
+# Age
+# City
+# name = "Ayush"
+# age=24
+# city="Mumbai"
+# print("My name is :",name)
+# print("My age is :",age)
+# print("My city of residence is :",city)
+
+# Q3.Create variables a = 15 and b = 30 and print:
+# The value of a is 15 and b is 30
+# a = 15 
+# b = 30
+# print("The value of a is", a,"and b is ", b)
+
+# Q4.Store your favorite number in a variable and print:
+# My favorite number is ___
+# fav = 4
+# print("My favurite number is ",fav)
+
+# Q5.Write a program to add two numbers:
+# a = 12
+# b = 8
+# print(a+b) 
+
+# Q6.Write a program to calculate the following:
+# a + b
+# a - b
+# a * b
+# a / b # where a = 20 , b = 5
+# a = 20
+# b = 5
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b) 
+
+# Q7.Write a program to calculate the remainder when 45 is divided by 7.
+# a=45
+# b=7
+# remainder=a%b
+# print(remainder) 
+
+# Q8.Calculate 5 to the power 3 using Python.
+# a=5
+# print(5**3) 
+
+# Q9.Create two numbers and calculate their average.
+# a=10
+# b=5
+# average=(a+b)/2
+# print(average) 
+
+# Q10.Convert the string "10" to an integer and add it with 5.
+# a=int("10")
+# print(a+5) 
+
+# Q11.Convert the number 100 into a string and print its datatype.
+# num=str(100)
+# print(type(num)) 
+
+# Q12.Convert "3.14" into a float and print the datatype.
+# a=float("3.14")
+# print(type(a)) 
+
+#MEDIUM 
+
+# Q13.Check whether 25 is greater than 30 and print the result.
+# print(25>30)
+
+# # Q14.Create variables
+# # a = 40 # b = 40
+# # Check if they are equal.
+# a=40
+# b=40
+# print(a==b) 
+
+# # Q15.Check whether 50 is not equal to 60.
+# print(50!=60)
+
+# # Q16.Check whether 100 is greater than or equal to 90.
+# print(100>=90)  
+
+# # Q17. Create
+# # a = 20# b = 10
+# # Check:
+# # (a > b) and (b > 5)
+# # Check:
+# # (a > b) or (b > 50)
+# a=20
+# b=10
+# print((a>b) and (b>5))
+# print((a>b) or (b>50))
+
+# # Q18.Check:
+# # not(a < b)
+# # where # a = 5 # b = 10
+# a=5 
+# b=10
+# print(not(a < b))
+
+# # Q19.Create
+# # num = 10 and add 5 using += and print the result.
+# num = 10
+# num += 5
+# print(num) 
+
+# # Q20.Create
+# # num = 50 and subtract 20 using -= and print the result.
+# num = 50 
+# num -= 20
+# print(num) 
+
+# # Q21.Create
+# # num = 8 and multiply it by 3 using *=.
+# num=8
+# num*=3
+# print(num) 
+
+# # Q22.Add the following numbers and print the result along with the datatype:
+# # a = 5 # b = 2.5
+# a = 5
+# b = 2.5
+# sum = a+b
+# print(sum)
+# print(type(sum)) 
+
+#HARD
+
+# Q23.Create:
+# a = "5" b = "10"
+# Print a + b.
+# Then convert them to integers and add them again.
+# a ="5"
+# b="10"
+# print(a+b)
+# a=int("5")
+# b=int("10")
+# print(a+b) 
+
+# Q24.Write a program to calculate the area of a rectangle:
+# length = 10 # width = 5 
+# length=10
+# width=5
+# area=length*width
+# print("Area of the rectangle is",area) 
+
+# Q25.Write a program to calculate the square of a number:
+# num = 8
+# print(num**2) 
+
+# Q26.Write a program to check whether 15 is divisible by 3.
+# print(15%3==0)  
+
+# Q27.Create variables:
+# x = 10 # y = 20 # z = 30
+# Check whether: # z > y and y > x
+# x = 10 
+# y = 20
+# z = 30
+# print(z > y and y > x ) 
+
+# Q28. Write a program to swap two numbers:
+# a = 5
+# b = 10
+# temp = a 
+# a = b 
+# b = temp 
+# print (a,b) 
---
+++ Day_2.py
@@ -0,0 +1,88 @@
+# DAY - 2 
+# Q1.Write a program that asks the user to enter their name and prints:
+# Welcome <name>
+# name=input("Enter your name :")
+# print("Your username is ",name) 
+
+# Q2.Write a program that takes any value from the user and prints:
+# the value & its datatype.
+# value=input("Enter any value: ")
+# print(type(value),value)   
+
+# Q3.Write a program that asks the user to enter a number and prints the number along with its datatype.
+# value=int(input("Enter any value: "))
+# print(type(value),value)
+
+# Q4.Write a program that takes the following inputs from the user:
+# name age marks
+# Then print them in a clear format.
+# name = input("Enter your name :")
+# age = input("Enter your age :")
+# marks = input("Enter your marks :")
+# print("My name is",name,"age being",age,"have scored",marks) 
+
+# Q5.Write a program that asks the user to enter two numbers and prints their sum.
+# num_1=int(input("Enter any number: "))
+# num_2=int(input("Enter any number: "))
+# print("Sum = ",num_1+num_2)
+
+# Q6.Write a program that asks the user to enter one number and prints its square.
+# num = int(input("Enter a number:"))
+# print("Squared value:",num**2)
+
+# Q7.Write a program that asks the user to enter two numbers and prints their average.
+# num_1=int(input("Enter any number: "))
+# num_2=int(input("Enter any number: "))
+# avg=(num_1+num_2)/2
+# print("Average = ",avg) 
+
+# Q8.Write a program that asks the user to enter the side of a square and prints the area of the square.
+side = int(input("Enter side of the square :"))
+print("Area = ",side**2) 
+# MEDIUM
+
+# Write a program that takes two numbers from the user and prints:
+
+# their sum
+
+# their difference
+
+# their product
+
+# their division.
+
+# Write a program that takes two numbers a and b from the user and prints whether a is greater than b.
+
+# Write a program that takes two numbers a and b from the user and prints whether a is greater than or equal to b.
+
+# Write a program that takes two floating numbers from the user and prints their average.
+
+# Write a program that asks the user to enter length and width of a rectangle and prints its area.
+
+# Write a program that asks the user to enter a number and prints:
+
+# The cube of the number is ___
+
+# Write a program that asks the user to enter two numbers and prints the remainder when the first number is divided by the second number.
+
+# HARD
+
+# Write a program that asks the user to enter three numbers and prints their average.
+
+# Write a program that asks the user to enter two numbers and prints True if the first number is divisible by the second number, otherwise False.
+
+# Write a program that asks the user to enter three numbers and prints the largest number among them using relational operators.
+
+# Write a program that asks the user to enter:
+
+# name
+
+# age
+
+# city
+
+# Then print a sentence like:
+
+# My name is ___, I am ___ years old and I live in ___.
+
+# Write a program that takes two numbers as strings, converts them into integers, and prints their sum.
