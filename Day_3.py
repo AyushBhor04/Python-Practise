ea45845e5521ad2464ecd1375f2e79b3d144bf96
Ayush Bhor <ayushbhor04@gmail.com>
2026-04-07 22:12:22 +0530

    1st commit for day - 3

--- Day_3.py
+++ Day_3.py
@@ -1,1 +1,42 @@
-# DAY - 3 
+# # DAY - 3 
+
+#EASY
+# Q1.Create a string and print it using: # double quotes # single quotes # triple quotes
+# str_1="Ayush Bhor"
+# str_2='Ayush Bhor'
+# str_3='''Ayush Bhor'''
+# print(str_1)
+# print(str_2) 
+# print(str_3)
+
+# Q2.Write a string that contains an apostrophe (') and print it correctly.
+# str_1="Ayush's Bhor"
+# print(str_1) 
+
+# Q3.Print a string in two lines using \n.
+# str_1="My name is Ayush\nI am 24 year old" 
+# print(str_1) 
+ 
+# Q4.Create two strings:
+# "Hello" # "World"
+# Concatenate them and print the result.
+# str_1="Hello"
+# str_2="World"
+# str_3=str_1 + str_2 
+# print(str_3) 
+
+# Q5.Create a string with your first name and print its length.
+# name=input("Enter your name : ")
+# print(name)
+# print(len(name))  
+
+# Q6.Create two strings:
+# first name # last name
+# Concatenate them with a space in between and print the full name.
+str_1="Ayush"
+str_2="Bhor"
+str_3=str_1+" "+str_2
+print(len(str_3)) 
+
+# Q6.Given a string: # "Python"
+# Print: # first character # last character 
