3cff1c9ab0f1daf86a2acf13160d21475f3ffada
Ayush Bhor <ayushbhor04@gmail.com>
2026-04-15 19:57:21 +0530

    Latest update

--- Day_5.py
+++ Day_5.py
@@ -85,7 +85,7 @@
 # else:
 #     print("Not Palindrome") 
 
-#Q16. Take two lists and:merge them into one list
+#Q16.Take two lists and:merge them into one list
 # list1 = [1, 2, 3]
 # list2 = [4, 5, 6]
 # merged = list1 + list2
