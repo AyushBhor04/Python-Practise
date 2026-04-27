b97ce81f12f60783eb28f1f83905d9626c710363
Ayush Bhor <ayushbhor04@gmail.com>
2026-04-27 13:40:36 +0530

    Another update

--- Day_9.py
+++ Day_9.py
@@ -1,3 +1,34 @@
 #DAY-9
 
-print("this is the execution for the day 9 ")
+#EASY
+
+#Q1.Print numbers from 1 to N (user input).
+# n = int(input("Enter any number : "))
+# i = 1 
+# while (i<=n):
+#     print(i)
+#     i+=1
+# print("Numbers printed from 1 to",n)
+
+#Q2.Print numbers from N to 1.
+# n = int(input("Enter any number : "))
+# N=n
+# i = 1 
+# while (n>=i):
+#     print(n)
+#     n-=1
+# print("Numbers printed from",N, "to 1") 
+
+# Print all even numbers from 1 to 50.
+
+# 4
+
+# Print all odd numbers from 1 to 50.
+
+# 5
+
+# Print the sum of numbers from 1 to N.
+
+# 6
+
+# Print the multiplication table of a number (same as yours, but formatted nicely).
