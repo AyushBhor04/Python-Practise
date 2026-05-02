c816b55402c65dc91b3dbbeb74076cc720201754
Ayush Bhor <ayushbhor04@gmail.com>
2026-05-02 18:44:35 +0530

    Initial commit

---
+++ Day_11.py
@@ -0,0 +1,24 @@
+#DAY-11
+
+#EASY
+
+#Q1.Write a function to take two numbers and return their sum
+# def sum(a,b):
+#     print(a+b)
+# x=int(input("enter a value : "))
+# y=int(input("enter a value : "))
+# print(sum(x,y)) 
+
+#Q2.Write a function that prints "Hello, <name>" takes name as input
+# def show(abc):
+#     print("Hello ",abc)
+# xyz=input("Enter your name : ")
+# show(xyz) 
+
+#Q3.Write a function to return the square of a number
+
+#Q4.Write a function with default parameters multiply two numbers , default values should be used if no arguments passed
+
+#Q5.Write a function that takes a list returns its length (don’t use len() inside print directly)
+
+#Q6.Write a function that takes a list prints all elements in one line
