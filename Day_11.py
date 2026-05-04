964a316b68adacb0a1b54cb7383e563eb36aa410
Ayush Bhor <ayushbhor04@gmail.com>
2026-05-04 14:40:03 +0530

    Initial  update

--- Day_11.py
+++ Day_11.py
@@ -16,9 +16,20 @@
 # show(xyz) 
 
 #Q3.Write a function to return the square of a number
+# def square(a):
+#     print(a*a)
+# x=int(input("Enter any number : "))
+# square(x)  
 
 #Q4.Write a function with default parameters multiply two numbers , default values should be used if no arguments passed
+# def mul(a=7,b=8):
+#     print(a*b)
+# mul() 
+# x=int(input("Enter any number : "))
+# y=int(input("Enter any number : "))
+# mul(x,y)
 
 #Q5.Write a function that takes a list returns its length (don’t use len() inside print directly)
 
-#Q6.Write a function that takes a list prints all elements in one line
+
+#Q6.Write a function that takes a list prints all elements in one line 
