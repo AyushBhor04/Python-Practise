ba20e9d014dfb3b9941dd98f6bbffe2c1bee4dc8
Ayush <ayushbhor04@gmail.com>
2026-05-05 01:33:56 +0530

    Medium level codes solves

--- Day_11.py
+++ Day_11.py
@@ -30,6 +30,118 @@
 # mul(x,y)
 
 #Q5.Write a function that takes a list returns its length (don’t use len() inside print directly)
-
+# def get_length(lst):
+#     count = 0
+#     for _ in lst:
+#         count += 1
+#     return count
+# nums = [1, 2, 3, 4, 5]
+# print("Length:", get_length(nums))
 
 #Q6.Write a function that takes a list prints all elements in one line 
+# def print_list(lst):
+#     for item in lst:
+#         print(item, end=" ")
+# nums = [1, 2, 3, 4, 5]
+# print_list(nums)
+
+#MEDIUM
+
+#Q7.Write a function to find maximum of three numbers
+# def max(a,b,c):
+#     if(a>b and a>c):
+#         print(a,"is the greatest")
+#     elif(b>a and b>c):
+#         print(b,"is the greatest")
+#     else:
+#         print(c,"is the greatest")
+# x=int(input("Enter a number : "))
+# y=int(input("Enter a number : "))
+# z=int(input("Enter a number : "))
+# max(x,y,z) 
+
+#Q8.Write a function to check whether a number is even or odd return result 
+# def parity(num):
+#     if(num%2==0):
+#         print("Even")
+#     else:
+#         print("Odd")
+# a=int(input("Enter any number : "))
+# parity(a) 
+
+#Q9.Write a function to calculate factorial using loop
+# def fact(n):
+#     prod = 1
+#     for i in range(1, n + 1):
+#         prod = prod * i
+#     return prod
+# a = int(input("Enter any number: "))
+# xyz = fact(a)
+# print(xyz)
+
+#Q10.Write a function to count vowels in a string
+
+#Q11.Write a function to take a list return sum of all elements
+
+#Write a function to: 
+
+# check if a number is prime 
+
+# HARD (Function thinking + structure)
+# 14
+
+# Write a function to:
+
+# find second largest number in a list
+# 15
+
+# Write a function to:
+
+# remove duplicates from a list
+# 16
+
+# Write a function to:
+
+# return reversed version of a list
+# 17
+
+# Write a function to:
+
+# count frequency of elements in a list (use dictionary)
+# 18
+
+# Write a function to:
+
+# check if a string is palindrome 
+
+# RECURSION (Very important — don’t skip)
+# 19
+
+# Write a recursive function to:
+
+# print numbers from 1 to N
+# 20
+
+# Write a recursive function to:
+
+# calculate factorial
+# 21
+
+# Write a recursive function to:
+
+# calculate sum of first N numbers
+# 22
+
+# Write a recursive function to:
+
+# find nth Fibonacci number
+# 23
+
+# Write a recursive function to:
+
+# reverse a string
+# 24
+
+# Write a recursive function to:
+
+# count digits in a number
